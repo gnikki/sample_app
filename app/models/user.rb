d0690bbe9336dae8297b5c175210b273b8368cec
Julian Yon <gnikki@x-parrot.net>
2013-04-18 19:11:07 +0100

    Done 10.2.1 Augmenting the user show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,8 @@
 
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
+  has_secure_password
+  has_many :microposts, dependent: :destroy
 
   before_save { email.downcase! }
   before_save :create_remember_token
@@ -21,8 +23,6 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }
   validates :password_confirmation, presence: true
-
-  has_secure_password
 
   private
     
