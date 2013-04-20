877a6837320c135fd96305cac8083c29a7323d12
Julian Yon <gnikki@x-parrot.net>
2013-04-20 12:27:49 +0100

    Done 11.3 The status feed (yay!)

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,8 +31,7 @@ class User < ActiveRecord::Base
   validates :password_confirmation, presence: true
 
   def feed
-    # Unfinished
-    Micropost.where("user_id = ?", id)
+    Micropost.from_users_followed_by(self)
   end
 
   def following?(other_user)
